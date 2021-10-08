0c5c5301b1a9f3bdf4d1f428c392fc031bd06bfa
Atamurad <atamurad.babakulyyev@gmail.com>
2021-10-08 15:55:53 -0400

    create new colors file with different colors options

---
+++ components/infastructure/theme/colors.js
@@ -0,0 +1,17 @@
+export const colors = {
+  brand: {
+   
+    prime: '#ffeb00',
+    primary: {
+      100: '#ffeb00',
+      200: '#eedb00',
+      300: '#E3D322',
+      400: '#E9DD50',
+      500: '#bbac00',
+      600: '#E3D322',
+      700: '#E3D322',
+      800: '#FAE603',
+      900: '#EDE556',
+    },
+  },
+}
