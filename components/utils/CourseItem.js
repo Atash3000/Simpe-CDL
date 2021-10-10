491550a547af4229d3def28d7a5a3154fb29178c
Atamurad <atamurad.babakulyyev@gmail.com>
2021-10-10 08:45:13 -0400

    update styling of the component

--- components/utils/CourseItem.js
+++ components/utils/CourseItem.js
@@ -1,31 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, TouchableOpacity, Alert, Pressable } from 'react-native'
 import styled from 'styled-components'
-import { ProgressBar, Colors } from 'react-native-paper';
-
+import { ProgressBar, Colors } from 'react-native-paper'
 
 const CourseItem = (props) => {
+  const [progres, setProgres] = useState(0)
   return (
-    <Card>
-      <Title>{props.title}</Title>
-      <ProgressBar progress={0.4} style={{backgroundColor:'red',padding:10}} />
-    </Card>
+    <Pressable onPress={props.onPress}>
+      <Card>
+        <Title>{props.title}</Title>
+        <ProgressStatus>{progres}%</ProgressStatus>
+        <Progress color={'#fff'} progress={progres} />
+      </Card>
+    </Pressable>
   )
 }
 
 const Card = styled(View)`
-  background-color: blue;
-  width: 100%;
-  padding: 20px;
-  border-radius: 15px;
-  box-shadow: -7px  10px -3px ;
+  background-color: ${(props) => props.theme.colors.ui.black[600]};
+  padding: 10px 20px;
+  border-radius: 12px;
+  box-shadow: 0px 4px 4px black;
+  margin: 5px 0;
 `
 
 const Title = styled(Text)`
   font-size: 20px;
-  text-align: center;
-  background-color: yellow;
+  text-align: left;
+  margin: 5px 0;
+  color: #fff;
   text-transform: capitalize;
+`
+
+const ProgressStatus = styled(Text)`
+  color: white;
+  margin: 2px 0;
+`
+
+const Progress = styled(ProgressBar)`
+  height: 3px;
+  margin: 5px 0;
+
+  background-color: #f7f7f7;
 `
 
 export default CourseItem
