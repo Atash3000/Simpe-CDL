7c1f46c7251eb88b1238dfdee97d5743dad564d0
Atamurad <atamurad.babakulyyev@gmail.com>
2021-10-10 09:31:14 -0400

    create action function for user answers

--- store/actions/userActions.js
+++ store/actions/userActions.js
@@ -6,13 +6,12 @@ import {
   USER_SIGNUP_REQUEST,
   USER_SIGNUP_SUCCESS,
   USER_LOG_OUT,
+  USER_ANSWER_CORRECT,
+  USER_ANSWER_WRONG,
 } from '../constants/userConstants'
 import axios from 'axios'
 import { storeData } from '../storage/asyncStorage'
 import AsyncStorage from '@react-native-async-storage/async-storage'
-
-
-
 
 export const createNewUser = (userNumber) => async (dispatch) => {
   dispatch({
@@ -20,18 +19,18 @@ export const createNewUser = (userNumber) => async (dispatch) => {
     payload: userNumber,
   })
   try {
-    const {data} = await axios.post('http://172.20.10.2:5050/api/v1/users', {
+    const { data } = await axios.post('http://172.20.10.2:5050/api/v1/users', {
       phoneNumber: userNumber,
     })
     const number = data.data.phoneNumber
     const verificationCode = data.data.verificationNumber
-      dispatch({
-        type: USER_SIGNUP_SUCCESS,
-        number: number,
-        verificationCode: verificationCode,
-      })
+    dispatch({
+      type: USER_SIGNUP_SUCCESS,
+      number: number,
+      verificationCode: verificationCode,
+    })
 
-    await storeData('userInfo',{number,verificationCode})
+    await storeData('userInfo', { number, verificationCode })
   } catch (error) {
     const message =
       error.response && error.response.data.message
@@ -44,12 +43,10 @@ export const createNewUser = (userNumber) => async (dispatch) => {
   }
 }
 
-
-
-export const userLoggedInStatus = (phoneNumber) =>async(dispatch)=> {
+export const userLoggedInStatus = (phoneNumber) => async (dispatch) => {
   dispatch({
     type: USER_LOGGED_IN_REQUEST,
-    payload:phoneNumber
+    payload: phoneNumber,
   })
   try {
     const { data } = await axios.get(
@@ -58,27 +55,42 @@ export const userLoggedInStatus = (phoneNumber) =>async(dispatch)=> {
     dispatch({
       type: USER_LOGGED_IN_SUCCESS,
       phoneNumber: data.data.user.phoneNumber,
-      verificationCode : data.data.user.verificationNumber
+      verificationCode: data.data.user.verificationNumber,
     })
   } catch (error) {
-     const message =
+    const message =
       error.response && error.response.data.message
         ? error.response.data.message
         : error.message
 
     dispatch({
       type: USER_LOGGED_IN_FAIL,
-      payload:message
+      payload: message,
     })
   }
-  
 }
 
-
-export const logout = () => async(dispatch) => {
- await AsyncStorage.removeItem('userInfo')
+export const logout = () => async (dispatch) => {
+  await AsyncStorage.removeItem('userInfo')
   dispatch({
     type: USER_LOG_OUT,
-    payload:{}
+    payload: {},
   })
+}
+
+export const checkUserAnswer = (answer, correctAnswer,hint) => async(dispatch) => {
+  if (answer === correctAnswer) {
+    dispatch({
+      type: USER_ANSWER_CORRECT,
+      payload: answer,
+    })
+       await storeData('correctAnswers', [answer])
+  } else {
+    dispatch({
+      type: USER_ANSWER_WRONG,
+      payload: hint,
+    })
+       await storeData('wrongAnswers', [answer])
+
+  }
 }
