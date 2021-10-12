29e8baa71015e5b457bb3b4075ed4a08543471f9
Atamurad <atamurad.babakulyyev@gmail.com>
2021-10-12 17:35:59 -0400

    Download google fonts to project using expo-google-fonts

--- App.js
+++ App.js
@@ -10,7 +10,32 @@ import Root from './Root'
 import Logout from './components/utils/Logout'
 import { ThemeProvider } from 'styled-components'
 import { theme } from './components/infastructure/theme'
+import {
+  useFonts as useMontserrat,
+  Montserrat_100Thin,
+  Montserrat_300Light,
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+  Montserrat_600SemiBold,
+  Montserrat_700Bold,
+  Montserrat_900Black,
+} from '@expo-google-fonts/montserrat'
+import AppLoading from 'expo-app-loading'
+
 const App = () => {
+  const [montserratLoaded] = useMontserrat({
+    Montserrat_100Thin,
+    Montserrat_300Light,
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_600SemiBold,
+    Montserrat_700Bold,
+    Montserrat_900Black,
+  })
+
+  if (!montserratLoaded) {
+   return  <AppLoading/>
+  }
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
