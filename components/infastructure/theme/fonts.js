b7fffe971ba2f2105509d662e617c342a123adb0
Atamurad <atamurad.babakulyyev@gmail.com>
2021-10-12 17:36:26 -0400

    add a function for responsive design for each mobile screen size

--- components/infastructure/theme/fonts.js
+++ components/infastructure/theme/fonts.js
@@ -7,6 +7,47 @@ export const fontWeight = {
   600: '600',
   700: '700',
   800: '800',
-  900:'900'
-  
+  900: '900',
+}
+
+export const fonts = {
+  body: {
+    thin: 'Montserrat_100Thin',
+    light:'Montserrat_300Light',
+    regular: 'Montserrat_400Regular',
+    medium: 'Montserrat_500Medium',
+    semiBold: "Montserrat_600SemiBold",
+    bold:"Montserrat_700Bold",
+    extraBold: 'Montserrat_900Black ',
+  },
+}
+
+export const fontSize = {
+  0: '0px',
+  2: '2px',
+  4: '4px',
+  6: '6px',
+  8: '8px',
+  10: '10px',
+  12: '12px',
+  14: '14px',
+  16: '16px',
+  18: '18px',
+  20: '20px',
+  22: '22px',
+  24: '24px',
+  26: '26px',
+  28: '28px',
+  30: '30px',
+  32: '32px',
+  34: '34px',
+  36: '36px',
+  38: '38px',
+  40: '40px',
+  42: '42px',
+  44: '44px',
+  46: '46px',
+  48: '48px',
+  52: '52px',
+  54: '54px',
 }
